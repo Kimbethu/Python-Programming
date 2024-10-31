d01f821bb46a9211c3af350fcc66a980285c5aaf
Kimbethu <bethukim@yahoo.com>
2024-10-31 18:30:42 +0300

    Variables

--- user_input.py
+++ user_input.py
@@ -2,4 +2,4 @@ name = input("Please enter your name: ")
 age = input("Please enter your age: ")
 location = input("Please enter your location: ")
 
-print("Hello {name}, you are {age} years old and live in {location}.")
+print(f"Hello {name}, you are {age} years old and live in {location}.")
